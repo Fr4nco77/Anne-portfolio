27864a877a7e66b80a063eb58300bf889abf7784
Franco Carreras <fcarreras777@gmail.com>
2025-10-10 22:30:49 -0300

    textos de la landing

--- src/i18n/ui/en.ts
+++ src/i18n/ui/en.ts
@@ -1,3 +1,10 @@
 export default {
-    
+  main: {
+    description:
+      "Discover the world of Anne Dupont, an international model with a unique career in the fashion industry. On her official portfolio, you'll find everything you need to get to know her in depth: her personal story, professional measurements, photo gallery, featured campaigns, collaborations with renowned brands, and much more.",
+    work: "Professional Model",
+    h1: "The essence of fashion in motion",
+    p: "Professional model who combines style & authenticity, creating images that connect with brands and audiences.",
+    button: "MY WORK",
+  },
 };
--- src/i18n/ui/es.ts
+++ src/i18n/ui/es.ts
@@ -1,3 +1,10 @@
 export default {
-    
+  main: {
+    description:
+      "Descubre el universo de Anne Dupont, modelo internacional con una trayectoria única en el mundo de la moda. En su portafolio oficial encontrarás todo lo que necesitas para conocerla en profundidad: su historia personal, medidas profesionales, galería de fotos, campañas destacadas, colaboraciones con marcas reconocidas y mucho más.",
+    work: "Modelo Profesional",
+    h1: "La esencia de la moda en movimiento",
+    p: "Modelo profesional que combina estilo & autenticidad, creando imágenes que conectan con marcas y audiencias.",
+    button: "MI TRABAJO",
+  },
 };
